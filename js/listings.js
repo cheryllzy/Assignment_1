8b047d42ab764d1c789d8f4f445c2ad6b5270bb6
cheryllzy <cheryllzy0404@gmail.com>
2023-11-27 17:46:11 +0800

    Listings Page Update 2 (Filter and Search Bar)

--- js/listings.js
+++ js/listings.js
@@ -17,3 +17,19 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 });
+
+function search() {
+    let input = document.getElementById('searchbar__input').value.toLowerCase();
+    let properties = document.getElementsByClassName('property');
+
+    for (let i = 0; i < properties.length; i++) {
+        let property = properties[i];
+        let details = property.getElementsByClassName('listing__details')[2].innerText.toLowerCase();
+
+        if (!details.includes(input)) {
+            property.style.display = "none";
+        } else {
+            property.style.display = "block";
+        }
+    }
+}

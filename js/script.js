ae272a8017e5bf5497f87d9740d2da0598741774
cheryllzy <cheryllzy0404@gmail.com>
2023-12-01 17:18:09 +0800

    Responsive Mobile View, 2 pages removed

--- js/script.js
+++ js/script.js
@@ -13,3 +13,12 @@ function search() {
         }
     }
 }
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
